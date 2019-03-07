32c71741d2831968bfc96508695f92adf0ddbbb7
Russell Kranz <russkranz@yahoo.com>
2019-03-07 10:19:04 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}::: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  //always need callback err for appendFile function.
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     maintenanceMsg: 'Sorry the website is under construction.'
+//   });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getYear', () => {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello express.</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'This is your welcome message.'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'Unable to fulfill request.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
